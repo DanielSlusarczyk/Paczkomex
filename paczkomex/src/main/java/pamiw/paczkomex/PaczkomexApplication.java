2995869611b1c4717d008cb2ce9dfa7429afa54c
Daniel <daniel00150015@gmail.com>
2022-10-29 18:06:32 +0000

    Spring Initializr

---
+++ paczkomex/src/main/java/pamiw/paczkomex/PaczkomexApplication.java
@@ -0,0 +1,13 @@
+package pamiw.paczkomex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaczkomexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaczkomexApplication.class, args);
+	}
+
+}
---
+++ paczkomex/src/test/java/pamiw/paczkomex/PaczkomexApplicationTests.java
@@ -0,0 +1,13 @@
+package pamiw.paczkomex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaczkomexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
